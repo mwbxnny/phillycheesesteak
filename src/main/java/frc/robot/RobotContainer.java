ecaf9686239b1267cfa71ffea06dc583992661bd
Madeline <maddiedoggy5@gmail.com>
2024-10-11 18:34:59 -0700

    robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Subsystems.Amp.Amp;
 import frc.robot.Subsystems.Arm.Arm;
@@ -14,17 +16,25 @@ import frc.robot.Subsystems.Intake.Intake;
 import frc.robot.Subsystems.Shooter.Shooter;
 
 public class RobotContainer {
+  private final CommandXboxController controller = new CommandXboxController(0);
+  private final Amp amp = new Amp();
+  private final Arm arm = new Arm();
+  private final Indexer indexer = new Indexer();
+  private final Intake intake = new Intake();
+  private final Shooter shooter = new Shooter();
+  
   public RobotContainer() {
-    final CommandXboxController controller = new CommandXboxController(0);
-    final Amp amp = new Amp();
-    final Arm arm = new Arm();
-    final Indexer indexer = new Indexer();
-    final Intake intake = new Intake();
-    final Shooter shooter = new Shooter();
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.x().whileTrue(new RunCommand(() -> indexer.runIndexer(2)));
+    controller.y().whileTrue(new RunCommand(() -> shooter.setVelocity(10, 1)));
+    controller.a().onTrue(new InstantCommand(() -> intake.requestSetpoint(20)));
+    controller.leftBumper().whileTrue(new RunCommand(() -> intake.requestIntakeVoltage(2)));
+    controller.rightBumper().onTrue(new InstantCommand(() -> arm.requestSetpoint(20)));
+    controller.b().whileTrue(new RunCommand(() -> amp.runAmp(2)));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
